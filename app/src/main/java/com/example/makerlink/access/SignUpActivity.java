db44e2f45cbdd15642cce172355b77ed0f645970
Martin Pedata <martin.pedata@hydroteam.be>
2025-04-26 23:10:41 +0200

    Login fully working

--- app/src/main/java/com/example/makerlink/access/SignUpActivity.java
+++ app/src/main/java/com/example/makerlink/access/SignUpActivity.java
@@ -3,11 +3,13 @@ package com.example.makerlink.access;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,13 +30,31 @@ import org.json.JSONObject;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+
+///  DONT FORGET TO DO LOCATION ID
 
 public class SignUpActivity extends AppCompatActivity {
     private TextView signUpText;
     private SharedPreferences sharedPref;
-    private String nameInput;
     private RequestQueue requestQueue;
     private SharedPreferences.Editor editor;
+
+    private EditText pw;
+    private String passwordInput;
+    private EditText un;
+    private String usernameInput;
+    private EditText ld;
+    private String lenderInput;
+    private EditText age;
+    private String ageInput;
+    private EditText name;
+    private String nameInput;
+    private EditText addr;
+    private String addressInput;
+    private String[] credentials = new String[6];
+    private int result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,61 +67,158 @@ public class SignUpActivity extends AppCompatActivity {
 //        });
         sharedPref = getSharedPreferences("myPref", MODE_PRIVATE);
         editor = sharedPref.edit();
+        result = 0;
     }
 
     public void goToHome(View view) throws UnsupportedEncodingException {
 
-        EditText pw = findViewById(R.id.password);
-        String passwordInput = pw.getText().toString();
-        EditText un = findViewById(R.id.username);
-        String usernameInput = un.getText().toString();
-        EditText em = findViewById(R.id.email);
-        String emailInput = em.getText().toString();
-        EditText age = findViewById(R.id.age);
-        String ageInput = age.getText().toString();
-        EditText name = findViewById(R.id.name);
+         pw = findViewById(R.id.password);
+         passwordInput = pw.getText().toString();
+         un = findViewById(R.id.username);
+         usernameInput = un.getText().toString();
+         ld = findViewById(R.id.lender);
+         lenderInput = ld.getText().toString();
+         age = findViewById(R.id.age);
+         ageInput = age.getText().toString();
+         name = findViewById(R.id.name);
         nameInput = name.getText().toString();
+        addr = findViewById(R.id.address);
+        addressInput = addr.getText().toString();
 
-        ///  CHANGE THIS BELOW:
+        credentials[0] = usernameInput;
+        credentials[1] = nameInput;
+        credentials[2] = ageInput;
+        credentials[3] = addressInput;
+        credentials[4] = lenderInput;
+        credentials[5] = passwordInput;
 
-        String address = "MariaTheresiastraat 82";
-        String lender = "yes";
-        int locationID = 0;
+        checkForValidity("https://studev.groept.be/api/a24pt215/AllUserInfo/" + usernameInput);
+    }
 
-        SignUpValidity validity = new SignUpValidity(ageInput,nameInput,emailInput,passwordInput,usernameInput);
-        switch (validity.checkValidity()) {
-            case 0:
-                if (!usernameInput.isEmpty() && !passwordInput.isEmpty()) {
-                    String url = String.format("https://studev.groept.be/api/a24pt215/InsertNewUser/%s/%s/%s/%s/%d/%s/%s/%d/%s",
-                            URLEncoder.encode(usernameInput, StandardCharsets.UTF_8.toString()),
-                            URLEncoder.encode(nameInput, StandardCharsets.UTF_8.toString()),
-                            URLEncoder.encode(emailInput, StandardCharsets.UTF_8.toString()),
-                            URLEncoder.encode(ageInput, StandardCharsets.UTF_8.toString()),
-                            0,
-                            URLEncoder.encode(address, StandardCharsets.UTF_8.toString()),
-                            URLEncoder.encode(lender, StandardCharsets.UTF_8.toString()),
-                            locationID,
-                            URLEncoder.encode(passwordInput, StandardCharsets.UTF_8.toString()));
-                    signUpUser(url);
+    public void checkForValidity(String requestURL) {
+        System.out.println("inside validity");
+        requestQueue = Volley.newRequestQueue(this);
+        JsonArrayRequest submitRequest = new JsonArrayRequest(Request.Method.GET, requestURL, null,
+                new Response.Listener<JSONArray>() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        System.out.println("inside onResponse of validity check");
+                        try {
+                            /// IF doesn't fail, then there is already a user with the same username.
+                            JSONObject o = response.getJSONObject(0);
+                            un.setText("");
+                            un.setHint("Username taken");
+                            un.setTextColor(Color.RED);
+
+                            un.setHintTextColor(Color.RED);
+
+                            // Remove focus to show hints
+                            name.setError("");
+                            age.setError("");
+                            addr.setError("");
+                            ld.setError("");
+                            pw.setError("");
+                            un.setError("");
+                        }
+                        catch (JSONException e) {
+                            ///  Check if valid info.
+
+                            for (String x: credentials) {
+                                if (x.isEmpty()) {
+                                    result = 1;
+                                    break;
+                                }
+                            }
+                            if (credentials[2].charAt(2) != '-' && credentials[2].charAt(5) != '-') {
+                                result = 2;
+                            }
+                            if (credentials[1].contains(" ")) {
+                                result = 3;
+                            }
+                            if (!credentials[4].equals("Y") && !credentials[4].equals("N")) {
+                                result = 4;
+                            }
+                            System.out.println("about to enter switch");
+                            switch (result) {
+                                case 1:
+                                    name.setText("");
+                                    age.setText("");
+                                    addr.setText("");
+                                    ld.setText("");
+                                    pw.setText("");
+
+                                    name.setHint("Fill in all fields !");
+                                    name.setTextColor(Color.RED);
+
+                                    name.setError("");
+                                    age.setError("");
+                                    addr.setError("");
+                                    ld.setError("");
+                                    pw.setError("");
+                                    break;
+                                case 2:
+                                    age.setText("");
+                                    age.setText("Fill in correct format: DD-MM-YY !");
+                                    age.setTextColor(Color.RED);
+                                    age.setError("");
+                                    break;
+                                case 3:
+                                    name.setText("");
+                                    name.setHint("Only first name!");
+                                    name.setTextColor(Color.RED);
+                                    name.setError("");
+                                    break;
+                                case 4:
+                                    ld.setText("");
+                                    ld.setHint("Type 'Y' or 'N' ! ");
+                                    ld.setTextColor(Color.RED);
+                                    ld.setError("");
+                                    break;
+                                case 0:
+                                    /// LocationID still not defined. The URLEncoder is necessary because of certain formatting required for restFul calls to occur.
+                                    System.out.println("inside case 0");
+                                    String url = null;
+                                    try {
+                                        url = String.format("https://studev.groept.be/api/a24pt215/InsertNewUser/%s/%s/%s/%d/%s/%s/%d/%s",
+                                                URLEncoder.encode(usernameInput, StandardCharsets.UTF_8.toString()),
+                                                URLEncoder.encode(nameInput, StandardCharsets.UTF_8.toString()),
+                                                URLEncoder.encode(ageInput, StandardCharsets.UTF_8.toString()),
+                                                0,
+                                                URLEncoder.encode(addressInput, StandardCharsets.UTF_8.toString()),
+                                                URLEncoder.encode(lenderInput, StandardCharsets.UTF_8.toString()),
+                                                404,
+                                                URLEncoder.encode(passwordInput, StandardCharsets.UTF_8.toString()));
+                                    } catch (UnsupportedEncodingException ex) {
+                                        throw new RuntimeException(ex);
+                                    }
+                                    System.out.println("SIGNUP URL: " + url);
+                                    signUpUser(url);
+                                    break;
+                            }
+                        }
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(SignUpActivity.this, "Network error! Please try again.", Toast.LENGTH_SHORT).show();
+                        Log.e("DatabaseError", error.toString());
+                    }
                 }
-            case 1:
-                /// ...
-            case 2:
-                /// ...
-            case 3:
-                /// ...
-            default:
-                /// ...
-        }
+        );
+        requestQueue.add(submitRequest);
+
     }
     public void signUpUser(String requestURL) {
+        System.out.println("Inside signUpUser");
         requestQueue = Volley.newRequestQueue(this);
-        System.out.println("We're in signupUser");
-        JsonArrayRequest submitRequest = new JsonArrayRequest(Request.Method.GET, requestURL, null,
+        JsonArrayRequest submitRequest = new JsonArrayRequest(Request.Method.POST, requestURL, null,
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
-                        System.out.println("We're in onResponse");
+                        System.out.println("inside onResponse of signUpUser");
                         editor.putString("Name", nameInput).apply();
 
                         /// GO to navigation template
@@ -115,8 +232,7 @@ public class SignUpActivity extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.e("SIGNUP", "Status Code: " + error.networkResponse.statusCode);
-                        Log.e("SIGNUP", "Response Data: " + new String(error.networkResponse.data));
+                        Toast.makeText(SignUpActivity.this, "Network error! Please try again.", Toast.LENGTH_SHORT).show();
                     }
                 }
         );
