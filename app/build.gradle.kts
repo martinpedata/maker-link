22b1c0987e79a08a69c25b752d0bc2fef10b1d83
Martin Pedata <martin.pedata@hydroteam.be>
2025-04-27 16:49:08 +0200

    Start of home frag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.annotation)
     implementation(libs.volley)
     implementation(libs.legacy.support.v4)
+    implementation(libs.blurview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
