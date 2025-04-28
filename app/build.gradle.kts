ce4ffff17bd44b76c6bb2cd505262a60120a54f0
Martin Pedata <martin.pedata@hydroteam.be>
2025-04-28 18:50:39 +0200

    A few additional stuff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.makerlink"
-        minSdk = 35
+        minSdk = 31
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
