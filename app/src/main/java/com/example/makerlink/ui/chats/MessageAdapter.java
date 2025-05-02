54697cf5fda3107b138a619f71cd4b4c7aa79b58
Ergi-Hydro <ergi.durro@hydroteam.be>
2025-05-02 18:52:03 +0200

    Extended the chat function.

--- app/src/main/java/com/example/makerlink/ui/chats/MessageAdapter.java
+++ app/src/main/java/com/example/makerlink/ui/chats/MessageAdapter.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.makerlink.R;
+import com.example.makerlink.R; //hello
 
 import java.util.List;
 
