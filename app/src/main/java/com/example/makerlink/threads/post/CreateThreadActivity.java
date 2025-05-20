a9620ca64d4a662db811eeee2fd945386f257da6
Martin Pedata <martin.pedata@hydroteam.be>
2025-05-21 00:13:06 +0200

    top comment preview

--- app/src/main/java/com/example/makerlink/threads/post/CreateThreadActivity.java
+++ app/src/main/java/com/example/makerlink/threads/post/CreateThreadActivity.java
@@ -1,7 +1,7 @@
 package com.example.makerlink.threads.post;
 
 
-//TODO: ADD COMMENTS. Add points to orders as well.
+//TODO: Add points to orders as well.
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
