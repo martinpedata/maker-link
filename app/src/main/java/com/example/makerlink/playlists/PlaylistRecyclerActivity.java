79fc1d3e5a91a92c69652b1bcd7f6b6061deabb1
Martin Pedata <martin.pedata@hydroteam.be>
2025-05-21 19:49:36 +0200

    able to delete playlist and threads + bin animation.

--- app/src/main/java/com/example/makerlink/playlists/PlaylistRecyclerActivity.java
+++ app/src/main/java/com/example/makerlink/playlists/PlaylistRecyclerActivity.java
@@ -2,32 +2,47 @@ package com.example.makerlink.playlists;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.makerlink.R;
+import com.example.makerlink.threads.post.CreateThreadActivity;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PlaylistRecyclerActivity extends AppCompatActivity {
 
@@ -37,6 +52,7 @@ public class PlaylistRecyclerActivity extends AppCompatActivity {
     private SharedPreferences.Editor editor;
     private int user_ID;
     private TextView title;
+    private LinearLayout linearLayout;
     private ArrayList<PlaylistRecyclerModel> playlistsItems = new ArrayList<>();
     private RequestQueue requestQueue;
 
@@ -52,6 +68,12 @@ public class PlaylistRecyclerActivity extends AppCompatActivity {
         });
         recyclerView = findViewById(R.id.my_recycler_playlist);
         title = findViewById(R.id.headingRecyclerPlaylist);
+        linearLayout = findViewById(R.id.linearLayoutPlaylists);
+
+        ItemTouchHelper helper = new ItemTouchHelper(callback);
+        helper.attachToRecyclerView(recyclerView);
+
+        requestQueue = Volley.newRequestQueue(this);
 
         /// DEFINE ADAPTER IN ONCREATE, AND CREATE A FIELD ATTRIBUTE SUCH THAT ALL YOU HAVE TO DO IN THE METHOD BELOW IS UPDATE EXISTING ADAPTER
         playlistAdapter = new PlaylistRecyclerAdapter(PlaylistRecyclerActivity.this, playlistsItems);
@@ -73,8 +95,92 @@ public class PlaylistRecyclerActivity extends AppCompatActivity {
 
         setUpPlaylists("https://studev.groept.be/api/a24pt215/RetrievePlaylists/" + user_ID);
     }
+
+    ItemTouchHelper.SimpleCallback callback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            int position = viewHolder.getAdapterPosition(); // get swiped position
+            int playlistid = playlistsItems.get(position).getPlaylistID(); // get ID of swiped thread
+
+            Snackbar snackbar = Snackbar.make(linearLayout, "Thread Deleted!", Snackbar.LENGTH_LONG);
+            snackbar.show();
+
+            deletePlaylist("https://studev.groept.be/api/a24pt215/DeletePlaylist", playlistid);
+            playlistsItems.remove(position);
+            playlistAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView,
+                                @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY,
+                                int actionState, boolean isCurrentlyActive) {
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+            if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                View itemView = viewHolder.itemView;
+
+                Paint paint = new Paint();
+                paint.setColor(Color.RED);
+                paint.setAntiAlias(true);
+
+                // Calculate position of red circle
+                float circleRadius = 60f;
+                float circleCenterY = itemView.getTop() + (itemView.getHeight() / 2f);
+                float circleCenterX;
+
+                if (dX > 0) { // Swiping right
+                    circleCenterX = itemView.getLeft() + 100;
+                } else { // Swiping left
+                    circleCenterX = itemView.getRight() - 100;
+                }
+
+                // Draw red circle
+                c.drawCircle(circleCenterX, circleCenterY, circleRadius, paint);
+
+                // Draw white bin icon
+                Drawable icon = ContextCompat.getDrawable(recyclerView.getContext(), R.drawable.bin_icon); // use your icon name here
+                if (icon != null) {
+                    int iconSize = 60;
+                    int left = (int) (circleCenterX - iconSize / 2);
+                    int top = (int) (circleCenterY - iconSize / 2);
+                    int right = (int) (circleCenterX + iconSize / 2);
+                    int bottom = (int) (circleCenterY + iconSize / 2);
+                    icon.setBounds(left, top, right, bottom);
+                    icon.setTint(Color.WHITE);
+                    icon.draw(c);
+                }
+            }
+        }
+    };
+
+    public void deletePlaylist(String requestURL, int playlistid) {
+        StringRequest submitRequest = new StringRequest (Request.Method.POST, requestURL,  new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //Turn the progress widget off
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        }) { //NOTE THIS PART: here we are passing the parameter to the webservice, NOT in the URL!
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("id", String.valueOf(playlistid));
+                return params;
+            }
+        };
+
+        requestQueue.add(submitRequest);
+    }
     public void setUpPlaylists(String requestURL) {
-        requestQueue = Volley.newRequestQueue(this);
         playlistsItems.clear();
         JsonArrayRequest submitRequest = new JsonArrayRequest(Request.Method.GET,requestURL, null,
                 new Response.Listener<JSONArray>() {
--- app/src/main/java/com/example/makerlink/threads/list/UserThreadRecyclerActivity.java
+++ app/src/main/java/com/example/makerlink/threads/list/UserThreadRecyclerActivity.java
@@ -1,38 +1,54 @@
 package com.example.makerlink.threads.list;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.makerlink.R;
 import com.example.makerlink.threads.post.CreateThreadActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserThreadRecyclerActivity extends AppCompatActivity {
     private FloatingActionButton createThreadButton;
+    private LinearLayout linearLayout;
     private RecyclerView recyclerView;
     private TextView heading;
     private int userID;
@@ -57,6 +73,7 @@ public class UserThreadRecyclerActivity extends AppCompatActivity {
         heading = findViewById(R.id.headingRecyclerUser);
         createThreadButton = findViewById(R.id.addThread);
         recyclerView = findViewById(R.id.my_recycler_userThreads);
+        linearLayout = findViewById(R.id.linearLayout);
 
         SharedPreferences sharedPreferences = getSharedPreferences("myPref", MODE_PRIVATE);
         userID = sharedPreferences.getInt("user_ID", -1);
@@ -69,9 +86,15 @@ public class UserThreadRecyclerActivity extends AppCompatActivity {
             }
         });
 
+        ItemTouchHelper helper = new ItemTouchHelper(callback);
+        helper.attachToRecyclerView(recyclerView);
+
+        requestQueue = Volley.newRequestQueue(this);
+
         threadAdapter = new ThreadRecyclerViewAdapter(this, threadItems);
         recyclerView.setAdapter(threadAdapter);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
     }
 
 
@@ -81,10 +104,94 @@ public class UserThreadRecyclerActivity extends AppCompatActivity {
         setUpThread("https://studev.groept.be/api/a24pt215/RetrieveUserThreads/" + userID);
     }
 
+    ItemTouchHelper.SimpleCallback callback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            int position = viewHolder.getAdapterPosition(); // get swiped position
+            int threadId = threadItems.get(position).getID(); // get ID of swiped thread
+
+            Snackbar snackbar = Snackbar.make(linearLayout, "Thread Deleted!", Snackbar.LENGTH_LONG);
+            snackbar.show();
+
+            deleteThread("https://studev.groept.be/api/a24pt215/DeleteThread", threadId);
+            threadItems.remove(position);
+            threadAdapter.notifyDataSetChanged();
+        }
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView,
+                                @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY,
+                                int actionState, boolean isCurrentlyActive) {
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+            if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                View itemView = viewHolder.itemView;
+
+                Paint paint = new Paint();
+                paint.setColor(Color.RED);
+                paint.setAntiAlias(true);
+
+                // Calculate position of red circle
+                float circleRadius = 60f;
+                float circleCenterY = itemView.getTop() + (itemView.getHeight() / 2f);
+                float circleCenterX;
+
+                if (dX > 0) { // Swiping right
+                    circleCenterX = itemView.getLeft() + 100;
+                } else { // Swiping left
+                    circleCenterX = itemView.getRight() - 100;
+                }
+
+                // Draw red circle
+                c.drawCircle(circleCenterX, circleCenterY, circleRadius, paint);
+
+                // Draw white bin icon
+                Drawable icon = ContextCompat.getDrawable(recyclerView.getContext(), R.drawable.bin_icon); // use your icon name here
+                if (icon != null) {
+                    int iconSize = 60;
+                    int left = (int) (circleCenterX - iconSize / 2);
+                    int top = (int) (circleCenterY - iconSize / 2);
+                    int right = (int) (circleCenterX + iconSize / 2);
+                    int bottom = (int) (circleCenterY + iconSize / 2);
+                    icon.setBounds(left, top, right, bottom);
+                    icon.setTint(Color.WHITE);
+                    icon.draw(c);
+                }
+            }
+        }
+    };
+
+    public void deleteThread(String requestURL, int threadId) {
+
+
+        StringRequest submitRequest = new StringRequest (Request.Method.POST, requestURL,  new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("id", String.valueOf(threadId));
+                return params;
+            }
+        };
+
+        requestQueue.add(submitRequest);
+    }
+
     public void setUpThread(String requestURL) {
         System.out.println("inside setUpThread");
         threadItems.clear();  // <-- THIS IS ESSENTIAL!
-        requestQueue = Volley.newRequestQueue(this);
         JsonArrayRequest submitRequest = new JsonArrayRequest(Request.Method.GET,requestURL, null,
                 new Response.Listener<JSONArray>() {
                     @Override
