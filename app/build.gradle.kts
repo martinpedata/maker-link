774251ae4016b413a82d0e0ba9dd91a4f56ddb2a
Ergi-Hydro <ergi.durro@hydroteam.be>
2025-05-22 20:54:06 +0200

    worked on the settings fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.auth:google-auth-library-oauth2-http:0.24.0")
     implementation ("com.google.auth:google-auth-library-oauth2-http:1.19.0")
     implementation("com.google.api-client:google-api-client:1.32.1")
